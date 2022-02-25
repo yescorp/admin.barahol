91df7e0f5ccb1194f2096c43815ec7360dc98ed5
Yestay <yestai777@gmail.com>
2022-02-25 09:11:41 +0600

    test

--- scripts/admin_panel.js
+++ scripts/admin_panel.js
@@ -1,7 +1,26 @@
 var selected_product_type = "clothes";
 var categories;
+var images = [];
+var fileList;
 
 $(document).ready(function(){
+
+    const fileSelector = document.getElementById('select-images');
+  fileSelector.addEventListener('change', (event) => {
+    fileList = event.target.files;
+    console.log(fileList);
+  });
+
+    $("#loginX").click(function(){
+        loginEmail();
+    });
+
+    $("#create_product_submit").click(function(){
+        
+        if(selected_product_type == "clothes"){
+            createProductClothes();
+        }
+    });
 
     fetch('/scripts/categories.json')
   .then(response => response.json())
@@ -122,7 +141,7 @@ $(document).ready(function(){
     });
 });
 
-var materialCount = 2;
+var materialCount = 0;
 
 function addMaterial(){
     
@@ -141,11 +160,12 @@ function addMaterial(){
     
     $("#clothes_material").append(inp);
     materialCount++;
+    console.log(materialCount);
 }
 
 function removeLastMaterial(){
     materialCount--;
-    if(materialCount == 1){
+    if(materialCount == -1){
         materialCount++;
         return;
     }
@@ -237,4 +257,261 @@ function showShoes(){
             
             $("#subcategory").append(option); 
         }
+
+}
+
+async function loginEmail() {
+    var email = $("#typeEmailX").val();
+    var password = $("#typePasswordX").val();
+    
+    var data = {Identity: email, Password: password};
+
+    console.log(data);
+
+    var response = await fetch('https://api.barahol.kz/account/login/email', {
+                    method: 'POST',
+                    redirect: 'follow',
+                    headers: {
+                        "Accept": "application/json; charset=utf-8",
+                        "Content-Type": "application/json;charset=utf-8",
+                      },
+                    body: JSON.stringify(data)
+                });
+
+
+    if(response.status != 200){
+        return;
+    } else{
+        var responseData = (await response.json());
+    console.log(responseData);
+    console.log(response.status);
+
+    console.log(email + " " + password);
+    
+    localStorage.setItem("refreshToken", responseData["refreshToken"]);
+    localStorage.setItem("accessToken", responseData["accessToken"]);
+
+    $("#product_info").show();
+    $("#login_form").hide();
+
+    let testData = {
+        "serialNumber": "фыв",
+        "name": "test2",
+        "description": "testDescription",
+        "category": {
+            "name": "testCategory"
+        },
+        "clothesCategory": {"name": "T-Shirt"},
+        "merch": {
+            "name": "testMerch"
+        },
+        "season": {"name": "summer"},
+        "materials": [
+            {
+                "material": {"name":"testMaterial1"},
+                "percent": 0.23
+            },
+            {
+                "material": {"name": "testMaterial2"},
+                "percent": 0.43
+            }
+        ],
+        "color": {
+            "name": "testColor",
+            "hexCode": "#4566ff"
+        },
+        "availableSizes": [
+            {
+                "size": {
+                    "americanSize": "amsz",
+                    "russianSize": "rusz"
+                }
+            }
+        ],
+        "maxSeriesCount": 10,
+        "brand": {
+            "name": "testBrand",
+            "madeIn": "CH"
+        },
+        "price": 100.01,
+        "salesPrice": 33.44,
+        "endDate": "2022-03-08T00:00:00",
+        "style": "testStyle",
+        "sportType": {
+            "name": "testSport"
+        },
+        "pattern": {
+            "name": "testPattenr"
+        },
+        "images": [
+            "64base string"
+        ]
+    };
+
+    console.log(JSON.stringify(testData));
+
+    let response2 = await fetch("https://api.barahol.kz/product/add/clothes",
+    {
+        method: "POST",
+        headers:
+            {
+                "Accept": "application/json; charset=utf-8",
+                "Content-Type": "application/json;charset=utf-8",
+                "Authorization": "Bearer " + localStorage.getItem("accessToken")
+            },
+        redirect: "follow",
+        body: JSON.stringify(testData)
+    });
+
+    console.log(response2.status);
+    
+
+}
+}
+
+async function createProductClothes(){
+    images = [];
+    
+    var data = {};
+
+    data["serialNumber"] = $("#serial_number").val();
+    console.log("asd");
+    data["name"] = $("#product_name").val();
+    data["description"] = $("#product_description").val();
+    data["category"] = {name: ($("#category_gender").val())};
+    data["clothesCategory"] = 
+    {name: ($("#subcategory").val() != "Другое" 
+    ? $("#subcategory").val() : $("#other_subcategory").val())};
+
+    data["merch"] = {name: $("#select_merch").val()};
+    data["season"] = {name: $("#season").val()};
+    data["materials"] = [];
+
+    if(materialCount == 0){
+        return;
+    }
+
+    for(let i = 0; i < materialCount; i++){
+        let material = {name: $("#clothes_material" + i).val()};
+        data["materials"].push({
+            material, 
+            percent: parseFloat("0." + $("#clothes_material_percent" + i).val())
+    });
+    }
+
+    data["color"] = {
+        name: $("#product_color").val(),
+        hexCode: $("#color_hex").val()
+
+    };
+
+    data["availableSizes"] = [];
+
+    let clothes_size_checkboxes = $(".clothes_size_checkbox:checked");
+    for (let i = 0; i < clothes_size_checkboxes.length; i++){
+        data["availableSizes"].push({
+            size: {americanSize: clothes_size_checkboxes[i].value,
+            russianSize: "none"
+            }
+        });
+    }   
+    
+    data["maxSeriesCount"] = parseInt($("#max_series_count").val(), 10);
+    
+    data["brand"] = {};
+
+    let brand = $("#product_brand").val();
+    let manufacturer = $("#manufacturer").val();
+
+    if(brand == "Другое"){
+        data["brand"]["name"] = $("#other_brand").val();
+    }
+    else {
+        data["brand"]["name"] = brand;
+    }
+
+    data["brand"]["madeIn"] = manufacturer;
+
+    data["price"] = parseFloat($("#product_base_price").val() + ".05");
+    data["salesPrice"] = parseFloat($("#product_sales_price").val() + ".05");
+
+
+    data["endDate"] = $("#expiration_date").val() + "T00:00:00";
+
+    data["style"] = $("#product_style").val();
+
+    if(data["style"] == "Спортивный"){
+        data["sportType"] = {
+            name: $("#sport_type").val() != "Другое" ? $("#sport_type").val() : $("#other_sport_type").val() 
+        };
+    }
+
+    data["pattern"] = {
+        name: $("#product_pattern").val() 
+    };
+
+
+    for(let i = 0; i < fileList.length; i++){
+        let file = fileList[i];
+        if (file.type && !file.type.startsWith('image/')) {
+            console.log('File is not an image.', file.type, file);
+            continue;
+          }
+        
+          const reader = new FileReader();
+      
+          reader.addEventListener('load', (event) => {
+            images.push(event.target.result.replace(/^data:image\/(png|jpg);base64,/, ""));
+            data["images"] = images;
+          });
+
+          reader.addEventListener('progress', (event) => {
+            if (event.loaded && event.total) {
+              const percent = (event.loaded / event.total) * 100;
+              console.log(`Progress: ${Math.round(percent)}`);
+              $("#image-progress").text(`Progress: ${Math.round(percent)}`);
+            }
+          });
+      
+          reader.readAsDataURL(file);
+    }
+
+    
+
+    console.log(images);
+
+    console.log(data);
+    console.log(JSON.stringify(data));
+
+    let response = await fetch("https://api.barahol.kz/product/add/clothes",
+    {
+        method: "POST",
+        headers:
+            {
+                "Accept": "application/json; charset=utf-8",
+                "Content-Type": "application/json;charset=utf-8",
+                "Authorization": "Bearer " + localStorage.getItem("accessToken")
+            },
+        redirect: "follow",
+        body: JSON.stringify(data)
+    });
+}
+
+function getBase64Image(img) {
+    // Create an empty canvas element
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    // Copy the image contents to the canvas
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+
+    // Get the data-URL formatted image
+    // Firefox supports PNG and JPEG. You could check img.src to
+    // guess the original format, but be aware the using "image/jpg"
+    // will re-encode the image.
+    var dataURL = canvas.toDataURL("image/png");
+
+    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
 }
